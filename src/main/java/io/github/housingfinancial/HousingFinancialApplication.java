6404b541342b7b90b40fe836c311fd34f53c32dd
yanghun0070 <yanghun0070@gmail.com>
2020-03-24 20:50:48 +0900

    Initial commit

---
+++ src/main/java/io/github/housingfinancial/HousingFinancialApplication.java
@@ -0,0 +1,13 @@
+package io.github.housingfinancial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousingFinancialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousingFinancialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/housingfinancial/HousingFinancialApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.housingfinancial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousingFinancialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
